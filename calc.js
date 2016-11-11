2ed4e7ea558b92c00e35fd66714bf5967c8cffb9
Kevin Lin <kevinptt0323@gmail.com>
2016-11-11 14:49:29 +0800

    rename oper->cmd

--- calc.js
+++ calc.js
@@ -11,26 +11,27 @@ class Calc {
     this.buffer = [];
     this.base = base;
   }
-  exec(_oper) {
+  exec(_cmd) {
     return new Promise((resolve, reject) => {
-      let oper = _oper.split('-');
-      console.log(oper);
-      if (oper.length==1) {
-        switch(oper[0]) {
+      let cmd = _cmd.split('-');
+      console.log(cmd);
+      if (cmd.length==1) {
+        switch(cmd[0]) {
           case 'ce':
             this.reset();
             resolve(0);
           case 'c':
           case 'bs':
           default:
-            reject(oper);
+            reject(cmd);
             return;
         }
-      } else if (isVal(oper)) {
+      } else if (isVal(cmd)) {
         if (this.buffer.length==0 || !isVal(this.buffer[this.buffer.length-1])) this.buffer.push(['val', 0]);
         let last = this.buffer[this.buffer.length-1];
-        last[1] = last[1] * this.base + parseInt(oper[1], this.base);
+        last[1] = last[1] * this.base + parseInt(cmd[1], this.base);
         resolve(last[1]);
+      } else if (isOper(cmd)) {
       }
     });
   }
